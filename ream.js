529085f90ee08aa76560615b3f158fd632e93d11
Chih-Ming Louis Lee <louis@chihminglee.com>
2021-04-26 23:10:00 -0500

    rename to ream

--- ream.js
+++ ream.js
@@ -150,7 +150,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('ream_core_bg.wasm', import.meta.url);
+        input = new URL('ream_bg.wasm', import.meta.url);
     }
     const imports = {};
 
